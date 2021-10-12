536bbf1dc16c5f03d1b11f12b6ddb270277dc18e
Ian P <ian@aotc.us>
2021-10-11 22:32:59 -0400

    cleaned up script.py

--- scripts.py
+++ scripts.py
@@ -1,24 +1,26 @@
 import os
-import uvicorn
+import sys
+from typing import Any, List, Optional
 
+import uvicorn
 from dispike.creating.models.permissions import (
     ApplicationCommandPermissions,
     ApplicationCommandPermissionType,
     NewApplicationPermission,
 )
 from loguru import logger
+from pydantic import BaseSettings, Field
 
-from app.main import bot, bot_settings, collections
+from app.main import bot, collections
 
-# Hardcoded user ids ftw!
-user_ids = [
-    123435280870014976,  # NotOats
-    132205334520397826,  # Tuxide
-]
 
-role_ids = [
-    895861910199746591,  # Bot Tester @ GDN Hub
-]
+class DevSettings(BaseSettings):
+    guild_ids: Optional[List[int]] = Field(None, env="DISCORD_GUILD_IDS")
+    role_ids: Optional[List[int]] = Field(None, env="DISCORD_ROLE_ID")
+    user_ids: Optional[List[int]] = Field(None, env="DISCORD_USER_IDS")
+
+
+settings = DevSettings(_env_file=".env")
 
 
 def start():
@@ -39,25 +41,22 @@ def start():
 def create_commands():
     """Create (or updates) all the registered commands on discord"""
 
-    # Gather commands
-    commands = []
-    for col in collections:
-        commands.extend(col.command_schemas())
+    # Global context
+    if _is_global_context():
+        logger.info("SCRIPT: Creating global discord commands")
 
-    # Global create
-    if bot_settings.guild_ids is None:
-        logger.info("Creating global discord commands")
+        [bot.register(cmd) for cmd in _get_commands()]
 
-        for command in commands:
-            bot.register(command=command)
+    elif settings.guild_ids is not None:
+        for guild_id in settings.guild_ids:
+            logger.info(f"SCRIPT: Creating commands for guild {guild_id}")
 
-    # Guild create
+            for cmd in _get_commands():
+                bot.register(command=cmd, guild_only=True, guild_to_target=guild_id)
     else:
-        for guild_id in bot_settings.guild_ids:
-            logger.info(f"Creating commands on guild (id: {guild_id})")
-
-            for command in commands:
-                bot.register(command=command, guild_only=True, guild_to_target=guild_id)
+        logger.warning(
+            "SCRIPT: Failed to create commands. No global context or guild ids."
+        )
 
 
 def update_command_permissions():
@@ -65,60 +64,84 @@ def update_command_permissions():
         lambda id: ApplicationCommandPermissions(
             id=id, type=ApplicationCommandPermissionType.USER, permission=True
         ),
-        user_ids,
+        settings.user_ids,
     )
 
     group_perms = map(
         lambda id: ApplicationCommandPermissions(
             id=id, type=ApplicationCommandPermissionType.ROLE, permission=True
         ),
-        role_ids,
+        settings.role_ids,
     )
 
-    new_perms = NewApplicationPermission(permissions=[*user_perms, *group_perms])
+    permissions = NewApplicationPermission(permissions=[*user_perms, *group_perms])
 
-    # Loop guild ids
-    for guild_id in bot_settings.guild_ids:
-        logger.info(f"Updating permissions on guild(id: {guild_id})")
+    if _is_global_context():
+        # Currently no support via Dispike for global command permissions
+        logger.warning("SCRIPT: There's no support for global command permissions.")
 
-        # Pull commands from discord & update them
-        for command in bot.get_commands(guild_only=True, guild_id_passed=str(guild_id)):
-            bot.set_command_permission(
-                command_id=command, guild_id=guild_id, new_permissions=new_perms
-            )
+    elif settings.guild_ids is not None:
+        for guild_id in settings.guild_ids:
+            logger.info(f"SCRIPT: Creating commands for guild {guild_id}")
+
+            for cmd in _get_existing_commands(guild_id):
+                bot.set_command_permission(cmd, guild_id, permissions)
+
+    else:
+        logger.warning(
+            "SCRIPT: Failed to update permissions. No global context or guild ids."
+        )
 
 
 def delete_commands():
     """Deletes every registered command on discord"""
 
-    # Global delete
-    if bot_settings.guild_ids is None:
-        logger.info("Deleting all commands globally")
+    if _is_global_context():
+        logger.info("SCRIPT: Deleting global discord commands")
         logger.warning(
             "This will remove _all_ commands associated "
             "with this application on _every_ server."
         )
 
-        if yes_or_no("Confirm deletion"):
-            # Hope you like waiting an hour for global commands to refresh!
-            for command in bot.get_commands():
-                bot.delete_command(command)
+        if _yes_or_no("Confirm deletion"):
+            [bot.delete_command(cmd) for cmd in _get_existing_commands()]
 
-    # Guild delete
+    elif settings.guild_ids is not None:
+        for guild_id in settings.guild_ids:
+            logger.info(f"SCRIPT: Deleting commands for {guild_id}")
+
+            for cmd in _get_existing_commands(guild_id):
+                bot.delete_command(cmd, guild_only=True, guild_id_passed=guild_id)
     else:
-        for guild_id in bot_settings.guild_ids:
-            logger.info(f"Deleting commands on guild (id: {guild_id})")
-
-            for command in bot.get_commands(
-                guild_only=True, guild_id_passed=str(guild_id)
-            ):
-                bot.delete_command(command, guild_only=True, guild_id_passed=guild_id)
+        logger.warning(
+            "SCRIPT: Failed to update permissions. No global context or guild ids."
+        )
 
 
 # region Utils
 
 
-def yes_or_no(question: str) -> bool:
+def _is_global_context() -> bool:
+    return True if "global" in sys.argv else False
+
+
+def _get_commands() -> List[Any]:
+    commands = []
+
+    for collection in collections:
+        commands.extend(collection.command_schemas())
+
+    return commands
+
+
+def _get_existing_commands(guild_id: Optional[int] = None) -> List[Any]:
+    if guild_id is not None:
+        return bot.get_commands(guild_only=True, guild_id_passed=str(guild_id))
+
+    return bot.get_commands()
+
+
+def _yes_or_no(question: str) -> bool:
     while True:
         reply = str(input(question + " (y/n: )")).lower().strip()
         if reply[:1] == "y":
