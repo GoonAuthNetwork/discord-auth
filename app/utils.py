40b1f50afa329ae182f7896c1dc17be3226e61e7
Ian P <ian@aotc.us>
2022-02-10 23:27:10 -0500

    Temporary username validation change.

--- app/utils.py
+++ app/utils.py
@@ -1,9 +1,18 @@
-import re
+# import re
 
 
 def valid_sa_name(name: str) -> bool:
     if name is None:
         return False
+    # TODO: Fix name validation with userid alternative
+    # Ghetto validation
+    if len(name) < 3:
+        return False
 
-    matched = re.match("^[a-zA-Z0-9-_ ]{3,50}$", name)
-    return bool(matched)
+    if len(name) > 100:
+        return False
+
+    return True
+
+    # matched = re.match("^[a-zA-Z0-9-_ ]{3,50}$", name)
+    # return bool(matched)
